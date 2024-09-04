802c0c494367d8dd14b580997f47555450638e38
Lisa Guo <lguo25@gmail.com>
2024-09-04 13:52:07 -0400

    Create separate agent daemonsets on windows for container insights and application signals (#90)

--- integration-tests/amazon-cloudwatch-observability/validator/resourceCount_linuxonly_test.go
+++ integration-tests/amazon-cloudwatch-observability/validator/resourceCount_linuxonly_test.go
@@ -7,15 +7,38 @@
 package validator
 
 const (
-	// Services count for CW agent on Linux and Windows
-	serviceCountLinux   = 6
-	serviceCountWindows = 3
+	// Services count on Linux:
+	// - amazon-cloudwatch-observability-webhook-service
+	// - cloudwatch-agent
+	// - cloudwatch-agent-headless
+	// - cloudwatch-agent-monitoring
+	// - dcgm-exporter-service
+	// - neuron-monitor-service
+	serviceCountLinux = 6
 
-	// DaemonSet count for CW agent on Linux and Windows
-	daemonsetCountLinux   = 4
-	daemonsetCountWindows = 2
+	// Services count on Windows:
+	// - cloudwatch-agent-windows
+	// - cloudwatch-agent-windows-headless
+	// - cloudwatch-agent-windows-monitoring
+	// - cloudwatch-agent-windows-container-insights
+	// - cloudwatch-agent-windows-container-insights-headless
+	// - cloudwatch-agent-windows-container-insights-monitoring
+	serviceCountWindows = 6
 
-	// Pods count for CW agent on Linux and Windows
+	// DaemonSet count on Linux:
+	// - cloudwatch-agent
+	// - dcgm-exporter
+	// - fluent-bit
+	// - neuron-monitor
+	daemonsetCountLinux = 4
+
+	// DaemonSet count on Windows:
+	// - cloudwatch-agent-windows
+	// - cloudwatch-agent-windows-container-insights
+	// - fluent-bit-windows
+	daemonsetCountWindows = 3
+
+	// Pods count on Linux and Windows
 	podCountLinux   = 3
 	podCountWindows = 0
 )
