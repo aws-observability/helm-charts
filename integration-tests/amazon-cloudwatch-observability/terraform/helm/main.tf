b61f8ed002604c2f897380a32d046e872e96585a
Hyunsoo Kim <hsookim@amazon.com>
2025-06-24 12:06:57 -0400

    more updates

--- integration-tests/amazon-cloudwatch-observability/terraform/helm/main.tf
+++ integration-tests/amazon-cloudwatch-observability/terraform/helm/main.tf
@@ -120,7 +120,7 @@ resource "helm_release" "this" {
     {
       name  = "region"
       value = "${var.region}"
-    },`
+    },
     {
       name  = "clusterName"
       value = "${aws_eks_cluster.this.name}"
