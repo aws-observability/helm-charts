31c6c983a4798798570088653b0961831b8b4934
Hyunsoo Kim <884273+movence@users.noreply.github.com>
2025-06-25 13:52:12 -0400

    upgrade neuron monitor version to 1.5.1 (#219)

--- integration-tests/amazon-cloudwatch-observability/terraform/helm-windows/main.tf
+++ integration-tests/amazon-cloudwatch-observability/terraform/helm-windows/main.tf
@@ -204,14 +204,16 @@ resource "helm_release" "this" {
   namespace = "amazon-cloudwatch"
   create_namespace = true
   chart      = "${var.helm_dir}"
-  set {
-    name  = "region"
-    value = "${var.region}"
-  }
-  set {
-    name  = "clusterName"
-    value = "${aws_eks_cluster.this.name}"
-  }
+  set = [
+    {
+      name  = "region"
+      value = "${var.region}"
+    },
+    {
+      name  = "clusterName"
+      value = "${aws_eks_cluster.this.name}"
+    }
+  ]
 }
 
 resource "null_resource" "deployment_wait" {
