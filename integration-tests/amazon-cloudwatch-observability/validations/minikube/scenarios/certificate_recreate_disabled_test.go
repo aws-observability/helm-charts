e1425b0a2fb49ecf4425774d5e43380d7a80ad2b
Dominic Sciascia <dscia@amazon.com>
2025-09-22 20:08:49 +0000

    Add test

---
+++ integration-tests/amazon-cloudwatch-observability/validations/minikube/scenarios/certificate_recreate_disabled_test.go
@@ -0,0 +1,58 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package scenarios
+
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/aws-observability/helm-charts/integration-tests/amazon-cloudwatch-observability/util"
+	"github.com/aws-observability/helm-charts/integration-tests/amazon-cloudwatch-observability/validations/minikube"
+	"github.com/stretchr/testify/assert"
+)
+
+const certificateRecreateDisabledPath = "/tmp/test-certificate-recreate-disabled.bin"
+
+func TestCertificateRecreateDisabled_Save(t *testing.T) {
+	k8sClient, err := util.NewK8sClient()
+	assert.NoError(t, err)
+
+	data := retrieveCABundle(t, *k8sClient)
+	assert.NotNil(t, data)
+
+	os.WriteFile(certificateRecreateDisabledPath, data, 0644)
+}
+
+func TestCertificateRecreateDisabled_Compare(t *testing.T) {
+	k8sClient, err := util.NewK8sClient()
+	assert.NoError(t, err)
+
+	actualData := retrieveCABundle(t, *k8sClient)
+	assert.NotNil(t, actualData)
+
+	savedData, err := os.ReadFile(certificateRecreateDisabledPath)
+	assert.NoError(t, err)
+
+	assert.False(t, bytes.Equal(actualData, savedData))
+}
+
+func retrieveCABundle(t *testing.T, k8sClient util.K8sClient) []byte {
+	whs, err := k8sClient.ListMutatingWebhookConfigurations()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, whs.Items)
+
+	for _, item := range whs.Items {
+		if item.ObjectMeta.Name != minikube.WebhookName {
+			continue
+		}
+		assert.NotEmpty(t, item.Webhooks)
+		assert.GreaterOrEqual(t, len(item.Webhooks), 1)
+
+		// Grab the first CA bundle
+		data := item.Webhooks[0].ClientConfig.CABundle
+		return data
+	}
+	return nil
+}
---
+++ integration-tests/amazon-cloudwatch-observability/validations/minikube/scenarios/certificate_recreate_enabled_test.go
@@ -0,0 +1,38 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package scenarios
+
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/aws-observability/helm-charts/integration-tests/amazon-cloudwatch-observability/util"
+	"github.com/stretchr/testify/assert"
+)
+
+const certificateRecreateEnabledPath = "/tmp/test-certificate-recreate-disabled.bin"
+
+func TestCertificateRecreateEnabled_Save(t *testing.T) {
+	k8sClient, err := util.NewK8sClient()
+	assert.NoError(t, err)
+
+	data := retrieveCABundle(t, *k8sClient)
+	assert.NotNil(t, data)
+
+	os.WriteFile(certificateRecreateEnabledPath, data, 0644)
+}
+
+func TestCertificateRecreateEnabled_Compare(t *testing.T) {
+	k8sClient, err := util.NewK8sClient()
+	assert.NoError(t, err)
+
+	actualData := retrieveCABundle(t, *k8sClient)
+	assert.NotNil(t, actualData)
+
+	savedData, err := os.ReadFile(certificateRecreateEnabledPath)
+	assert.NoError(t, err)
+
+	assert.True(t, bytes.Equal(actualData, savedData))
+}
