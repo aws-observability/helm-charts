c56b16be6c8811b6e14f98720415d71b5be4ad40
Kaushik Surya <kausyas@amazon.com>
2026-04-10 16:31:53 +0000

    Revert "Scope config-map permission for cloudwatch-agent to namespace only (#287)"
    This reverts commit b12a6c5b21b1e41200c5f4c26ae9258470fe2bfa.

--- integration-tests/amazon-cloudwatch-observability/validations/eks/resources_generated_test.go
+++ integration-tests/amazon-cloudwatch-observability/validations/eks/resources_generated_test.go
@@ -127,9 +127,6 @@ func TestResourcesGenerated(t *testing.T) {
 	assert.True(t, exists)
 
 	// Validating Roles
-	exists, err = k8sClient.ValidateRoleExists(namespace, agentName+"-role")
-	assert.NoError(t, err)
-	assert.True(t, exists)
 	exists, err = k8sClient.ValidateRoleExists(namespace, dcgmExporterName+"-role")
 	assert.NoError(t, err)
 	assert.True(t, exists)
@@ -146,9 +143,6 @@ func TestResourcesGenerated(t *testing.T) {
 	assert.True(t, exists)
 
 	// Validating RoleBinding
-	exists, err = k8sClient.ValidateRoleBindingExists(namespace, agentName+"-role-binding")
-	assert.NoError(t, err)
-	assert.True(t, exists)
 	exists, err = k8sClient.ValidateRoleBindingExists(namespace, dcgmExporterName+"-role-binding")
 	assert.NoError(t, err)
 	assert.True(t, exists)
